fdb4ff65c8b3766d2eb85fd6331d7f912e2c3bba
minji <JHTA@JHTA-PC>
2020-07-02 14:15:18 +0900

    bootstrap.css수정

--- FinalProject_Allchwi/src/main/java/com/jhta/allchwi/controller/search/DetailSearchController.java
+++ FinalProject_Allchwi/src/main/java/com/jhta/allchwi/controller/search/DetailSearchController.java
@@ -1,13 +1,15 @@
 package com.jhta.allchwi.controller.search;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DetailSearchController {
 	
 	@PostMapping("/search/detailSearch")
-	public String search() {
+	public String search(String keyword, Model model) {
+		model.addAttribute("keyword",keyword);
 		return ".search.search";
 	}
 }
--- FinalProject_Allchwi/src/main/webapp/WEB-INF/views/search/search.jsp
+++ FinalProject_Allchwi/src/main/webapp/WEB-INF/views/search/search.jsp
@@ -1,3 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <h1>상세검색페이지</h1>
+<p>입력된 검색어 : ${keyword }</p>
