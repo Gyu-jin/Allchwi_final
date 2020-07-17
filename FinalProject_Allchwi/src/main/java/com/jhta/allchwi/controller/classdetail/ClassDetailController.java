a693291b0797c0f6fe87efac0f2c703be32a0f25
danny <ekdms348@gmail.com>
2020-07-17 17:50:36 +0900

    detail session추가

--- FinalProject_Allchwi/src/main/java/com/jhta/allchwi/controller/classdetail/ClassDetailController.java
+++ FinalProject_Allchwi/src/main/java/com/jhta/allchwi/controller/classdetail/ClassDetailController.java
@@ -2,6 +2,9 @@ package com.jhta.allchwi.controller.classdetail;
 
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,11 @@ import org.springframework.web.servlet.ModelAndView;
 import com.jhta.allchwi.service.classdetail.ClassQnaService;
 import com.jhta.allchwi.service.classdetail.ClassReviewService;
 import com.jhta.allchwi.service.classopen.ClassInfoService;
+import com.jhta.allchwi.service.login.MemberInfoService;
 import com.jhta.allchwi.vo.classdetail.ClassQnaVO;
 import com.jhta.allchwi.vo.classdetail.ClassReviewVO;
 import com.jhta.allchwi.vo.classopen.ClassInfoVO;
+import com.jhta.allchwi.vo.login.ProfileVO;
 
 
 
@@ -22,9 +27,15 @@ public class ClassDetailController {
 	private ClassInfoService classinfo_service;
 	@Autowired
 	private ClassReviewService classreview_service;
-	
+	private MemberInfoService mis;
 	@GetMapping("/classDetail/detail")
-	public ModelAndView detail(int class_num) {
+	public ModelAndView detail(int class_num,HttpServletRequest req) {
+		ServletContext sc=req.getSession().getServletContext();
+		if(req.getSession().getAttribute("ml_num") != null ) {
+			int ml_num = (int)req.getSession().getAttribute("ml_num");
+			ProfileVO pfv = mis.selectInfo(ml_num);
+			sc.setAttribute("mem", pfv);
+		}
 		ModelAndView mv=new ModelAndView(".classDetail.detail");
 		ClassInfoVO cvo=classinfo_service.getInfo(class_num);
 		List<ClassReviewVO> rlist=classreview_service.reviewList(class_num);
